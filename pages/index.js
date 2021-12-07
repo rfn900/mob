e50d88356e52dac2954f6f81f8a417169b0e9ada
rfn900 <rfn900@gmail.com>
2021-12-07 10:23:12 +0100

    Refactor with Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,20 @@
-import Head from "next/head";
-import Nav from "../components/homepage/Nav";
-import Hero from "../components/homepage/Hero";
-import AboutStats from "../components/homepage/AboutStats";
-import Carousel from "../components/homepage/Carousel";
-import About from "../components/homepage/About";
-import Brands from "../components/homepage/Brands";
-import ContactusSection from "../components/homepage/ContactusSection";
-import Footer from "../components/homepage/Footer";
+import Layout from '../components/layouts/Layout'
+import Hero from '../components/homepage/Hero'
+import AboutStats from '../components/homepage/AboutStats'
+import Carousel from '../components/homepage/Carousel'
+import About from '../components/homepage/About'
+import Brands from '../components/homepage/Brands'
+import ContactusSection from '../components/homepage/ContactusSection'
 
 export default function Home() {
   return (
-    <div className="w-screen overflow-hidden">
-      <Head>
-        <title>MARSian Oline Group</title>
-        <meta name="description" content="Homepage for MARSian Online Group" />
-        <link rel="icon" href="/MOG_logo.png" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.1/css/swiper.css"
-        />
-      </Head>
-      <Nav />
+    <Layout>
       <Hero />
       <AboutStats />
       <Carousel />
       <About />
       <Brands />
       <ContactusSection />
-      <Footer />
-    </div>
-  );
+    </Layout>
+  )
 }
